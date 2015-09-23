ad1573b5ee85a8794ad36b4aae18bfd3bc778d96
BonoNoFlow <hendrik.nieuwenhuis@gmail.com>
2015-09-23 03:08:03 +0200

    Initial Commit

---
+++ MpdClient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MpdClient
+//
+//  Created by hendrik nieuwenhuis on 23/09/15.
+//  Copyright © 2015 hendrik nieuwenhuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
